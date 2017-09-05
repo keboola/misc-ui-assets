700beaab1c7771d1700107975c0e7aa843f0f7c4
Vladimír Kriška <vlado@keboola.com>
2017-09-05 23:03:22 +0200

    Init

---
+++ build.php
@@ -0,0 +1,29 @@
+#!/usr/bin/env php
+<?php
+
+$configFile = __DIR__ . '/config.json';
+$json = file_get_contents($configFile);
+$config = json_decode($json, true);
+
+$distDir = __DIR__ . '/dist';
+$installDir = __DIR__ . '/src';
+
+$buildFile = <<<BASH
+#!/bin/bash
+
+set -e
+\n
+BASH;
+
+foreach ($config as $item) {
+    $libraryDir = sprintf('/%s/%s', $item['id'], $item['version']);
+
+    $buildFile .= sprintf('mkdir -p %s%s', $installDir, $libraryDir) . "\n";
+    $buildFile .= sprintf('cd %s%s', $installDir, $libraryDir) . "\n";
+    $buildFile .= sprintf('if [ -f package.json ]; then yarn; else yarn add %s@%s; fi', $item['id'], $item['version']) . "\n";
+
+    $buildFile .= sprintf('mkdir -p %s%s', $distDir, $libraryDir) . "\n";
+    $buildFile .= sprintf('cp -R %s%s%s/* %s%s', $installDir, $libraryDir, $item['dir'], $distDir, $libraryDir) . "\n";
+}
+
+echo $buildFile;
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Misc UI Assets
+
+Assets definition and few helper scripts.
+
+## Usage
+
+1. Modify `config.json` file
+2. Generate build script `docker-compose run --rm php sh -c './build.php > build.sh'`
+3. Check build script (`cat build.sh`)
+4. Run build script `docker-compose run --rm node sh -c 'cat build.sh | sh'`
+5. Commit changes
